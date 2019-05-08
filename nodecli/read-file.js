efbaffaf35378943be9c21fb2e0702cb9cd725fa
maeshiro <maeshiro@mfac.jp>
2019-05-08 11:53:57 +0900

    md-to-html

--- nodecli/read-file.js
+++ nodecli/read-file.js
@@ -1,8 +1,19 @@
 const program = require("commander");
 const fs = require("fs");
+const marked = require("marked");
+
+program
+  .option("--gfm", "GFMを有効にする")
+  .option("-S, --sanitize", "サニタイズを行う");
 
 program.parse(process.argv);
 const filePath = program.args[0];
+
+const markedOptions = {
+  gfm: false,
+  sanitize: false,
+  ...program.opts()
+};
 
 fs.readFile(filePath, "utf8", (err, file) => {
     if (err) {
@@ -10,5 +21,9 @@ fs.readFile(filePath, "utf8", (err, file) => {
         process.exit(err.code);
         return;
     }
-    console.log(file);
+    const html = marked(file, {
+      gfm: markedOptions.gfm,
+      sanitize: markedOptions.sanitize
+    });
+    console.log(html);
 });
--- nodecli/sample.md
+++ nodecli/sample.md
@@ -1,1 +1,9 @@
 # sample
+## TEST1
+- a
+- b
+- c
+
+## TEST2
+1. http://example.com/
+2. <p> aaaaaaaa </p>
